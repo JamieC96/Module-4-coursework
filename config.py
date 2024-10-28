025d162fa4e3ddcd0a7f9bc979845dec985bd91b
JamieC96 <152332511+JamieC96@users.noreply.github.com>
2024-10-28 19:06:28 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "postgresql://admin:O9TXTq5c31xclcOhBob1mDrPqyn4dvU5@dpg-csfsnktsvqrc739r60kg-a/axolotl_ya4x")
