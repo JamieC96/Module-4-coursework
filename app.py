575003276241e9a26054386ad6badae945913659
JamieC96 <152332511+JamieC96@users.noreply.github.com>
2024-10-28 19:32:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app= Flask(__name__, template_folder='templates')
 
 app.secret_key = 'cheese'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv 'postgresql://admin:admin@localhost:5432/axolotl'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv os.getenv 'DATABASE_URL 'postgresql://admin:admin@localhost:5432/axolotl'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
